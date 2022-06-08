3d286b18427d9455e9bb34c95031d770a6dc1ac7
ariw96 <ariw96@gmail.com>
2022-06-08 09:38:05 +0300

    add post option

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ let port = process.env.PORT || 4000;
 app.get('/', (req, res) => {
     res.send(importData);
 });
+app.post('/', (req, res) => {
+    console.log(req.body);
+ res.status(201).send(req.body);
+
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
